4c2a71145a0665a962d17540fc63d5bf244ecc15
DabAZ <dieboldhan123@gmail.com>
2023-09-12 20:55:14 +0800

    Add the create channel modal

---
+++ components/modals/create-channel-modal.tsx
@@ -0,0 +1,140 @@
+"use client"
+
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+import { FileUpload } from "@dabaz/components/file-upload";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@dabaz/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@dabaz/components/ui/form";
+import { Input } from "@dabaz/components/ui/input";
+import { Button } from "@dabaz/components/ui/button";
+import { useModal } from "@dabaz/hooks/use-modal-store";
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required to continue!"
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required to continue!"
+  })
+});
+
+export const CreateChannelModal = () => {
+  const { isOpen, onClose, type } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "createChannel";
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-neutral-100 dark:bg-neutral-900 text-black dark:text-white p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-medium tracking-normal">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-neutral-600 dark:text-neutral-400">
+            Give your server a personality with a name and an image. You can always change it later.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      className="tracking-wider text-sm text-neutral-600 dark:text-neutral-400"
+                    >
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="text-black dark:text-white"
+                        placeholder="Enter your server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-neutral-100 dark:bg-neutral-900 px-6 py-4">
+              <Button 
+                variant="primary"
+                disabled={isLoading}
+              >
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,7 @@ import { CreateServerModal } from "@dabaz/components/modals/create-server-modal"
 import { InviteModal } from "@dabaz/components/modals/invite-modal";
 import { EditServerModal } from "@dabaz/components/modals/edit-server-modal";
 import { MembersModal } from "@dabaz/components/modals/members-modal";
+import { CreateChannelModal } from "@dabaz/components/modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -24,6 +25,7 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   )
 }
